b7ade785a413ba83232da716024b25cd38b4c368
gtmcc <109885476+glenn-mccrea@users.noreply.github.com>
2026-02-24 19:10:16 -0600

    working on models

--- JavaBackend/src/main/java/com/HDT/JavaBackend/JavaBackendApplication.java
+++ JavaBackend/src/main/java/com/HDT/JavaBackend/JavaBackendApplication.java
@@ -1,13 +1,18 @@
 package com.HDT.JavaBackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        // Load .env file and push DB_PASSWORD into Spring
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(JavaBackendApplication.class, args);
+    }
 }
