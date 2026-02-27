2169d733380de2eb19501cf2377775862faad57f
gtmcc <109885476+glenn-mccrea@users.noreply.github.com>
2026-02-26 19:49:55 -0600

    strip 'fake-it' logic from loading page

--- react-final-project/src/components/LoadingPage/LoadingPage.jsx
+++ react-final-project/src/components/LoadingPage/LoadingPage.jsx
@@ -1,41 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./loading-page.css";
-import { Link } from "react-router";
 
 const LoadingPage = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 4000);
-
-    return () => clearTimeout(timer); /* This is a cleanup function. */
-  }, []);
-
   return (
     <main>
       <div id="loading-page-main-div">
-        {isLoading ? (
-          <div id="loading-statement">
-            <p>
-              <em>loading...</em>
-            </p>
-
-            <p className="loader"></p>
-          </div>
-        ) : (
-          <div id="no-logs-statement">
-            <p>Sorry, no log items to see here.</p>
-            <br />
-            <p>
-              To get started,
-              <Link to="/log" className="button-div">
-                Log Activity
-              </Link>
-            </p>
-          </div>
-        )}
+        <div id="loading-statement">
+          <p>
+            <em>loading...</em>
+          </p>
+          <p className="loader"></p>
+        </div>
       </div>
     </main>
   );
