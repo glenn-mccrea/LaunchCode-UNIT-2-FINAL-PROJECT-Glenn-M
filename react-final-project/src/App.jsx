9bf509cbd45e181388220ae0c78f3240fea3c0c7
gtmcc <109885476+glenn-mccrea@users.noreply.github.com>
2026-03-11 19:09:41 -0500

    remove unused err parameter

--- react-final-project/src/App.jsx
+++ react-final-project/src/App.jsx
@@ -81,7 +81,7 @@ function App() {
     fetch(`${API_URL}/api/shopping`)
       .then((res) => res.json())
       .then((data) => setShoppingItems(data))
-      .catch((err) =>
+      .catch(() =>
         setAppError("Could not load shopping items. Please try again."),
       );
   }, []);
